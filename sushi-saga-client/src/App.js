8018155e41457206c6901da3ee0cf02517f37cd7
lgm527 <laurellmccaffrey@gmail.com>
2019-09-03 09:57:19 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,44 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushi: [],
+    startIndex: 0,
+    eatenSushi: [],
+    wallet: 100
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res => res.json())
+    .then(sushiData => this.setState({sushi: sushiData}))
+  }
+
+  moreSushi = () => {
+    this.setState({
+      startIndex: this.state.startIndex+4
+    })
+  }
+
+  addPlate = (sushiObj) => {
+    this.setState({
+      eatenSushi: [...this.state.eatenSushi, sushiObj]
+    })
+  }
+
+  buyOneSushi = (price) => {
+    this.setState({
+      wallet: this.state.wallet-price
+    })
+  }
+
   render() {
+    const {sushi, startIndex, eatenSushi, wallet} = this.state
+    console.log(eatenSushi);
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushi={sushi} startIndex={startIndex} moreSushi={this.moreSushi} addPlate={this.addPlate} buyOneSushi={this.buyOneSushi} wallet={wallet}/>
+        <Table eatenSushi={eatenSushi} wallet={wallet}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,18 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
+
+  function renderSushis() {
+    return props.sushi.slice(props.startIndex, (props.startIndex+4)).map(sushiObj => <Sushi sushi={sushiObj} key={sushiObj.id} addPlate={props.addPlate} buyOneSushi={props.buyOneSushi} wallet={props.wallet}/>)
+  }
+
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        { renderSushis() }
+        <MoreButton moreSushi={props.moreSushi} />
       </div>
     </Fragment>
   )
