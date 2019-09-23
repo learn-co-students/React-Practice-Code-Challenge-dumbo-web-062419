5df051cfa8b5c9c424125c0aa27fb9410da95198
Dennis Wang <dwang0816@gmail.com>
2019-09-23 16:14:39 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,68 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state= {
+    sushiCollection: [],
+    sushiIndex: 0,
+    wallet: 100
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      const sushiData =data.map(sushi => {
+        sushi.eaten=false 
+        return sushi})
+      this.setState({sushiCollection: sushiData})
+    }) 
+
+  }
+
+  getSushis = () => {
+    
+    let sushiCollection = this.state.sushiCollection.slice(this.state.sushiIndex, this.state.sushiIndex + 4)
+    return sushiCollection
+  } 
+
+  handleMoreSushi = () => {
+    this.setState({
+    sushiIndex: this.state.sushiIndex + 4
+    })
+  }
+
+  handleEatSushi = (obj) => {
+    let updatedArray = this.state.sushiCollection.map(sushi => {
+      if  (sushi.id === obj.id ){
+        sushi.eaten = true
+
+        return sushi
+
+      }
+      else
+        return sushi
+
+    })
+
+    this.setState({
+      sushiCollection: updatedArray,
+      wallet: this.state.wallet-obj.price
+    })
+  }
+
+  getEatenSushis(){
+    return this.state.sushiCollection.filter(sushi => sushi.eaten)
+  }
+
+
+
+
   render() {
+    console.log(this.state.sushiCollection)
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer sushis ={this.getSushis()} handleMoreSushi={this.handleMoreSushi} handleEatSushi={this.handleEatSushi}/>
+        <Table sushis={this.getEatenSushis()} wallet={this.state.wallet}/>
       </div>
     );
   }
